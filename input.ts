fc7411217585328354a2ee35b466f7480136085e
Bronco Oostermeyer <b.oostermeyer@flusso.nl>
2020-12-03 09:33:56 +0100

    generic input read

--- input.ts
+++ input.ts
@@ -1,3 +1,7 @@
-export const input = [
-];
+import * as fs from 'fs';
 
+// "stolen" from 
+export const getInput = (inputFile = './input.txt', separator = "\n") => {
+    const file = fs.readFileSync(inputFile, "utf-8");
+    return file.split(separator).map(x => x);
+};
