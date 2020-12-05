415ea864edcda58dd0ce159547bb293ba25e6454
Bronco Oostermeyer <b.oostermeyer@flusso.nl>
2020-12-05 13:41:41 +0100

    removed blanks

--- day5/day5a_2.ts
+++ day5/day5a_2.ts
@@ -1,1 +1,1 @@
-console.log(Math.max(...((<string>(require('fs')).readFileSync('./input.txt', 'utf-8')).split('\n').map(x => parseInt(x.replace(/F|L/g, '0').replace(/B|R/g, '1'), 2)))));
+console.log(Math.max(...((<string>(require('fs')).readFileSync('./input.txt','utf-8')).split('\n').map(x => parseInt(x.replace(/F|L/g,'0').replace(/B|R/g,'1'), 2)))));
