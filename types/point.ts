b1acf8d049ec3065737a520e6fc78eedf509bc56
Bronco Oostermeyer <b.oostermeyer@flusso.nl>
2020-12-08 20:47:17 +0100

    added types/point

---
+++ types/point.ts
@@ -0,0 +1,18 @@
+
+import { Vector } from './vector';
+
+export class Point {
+    
+    constructor(public x = 0, public y = 0) { }
+
+    addVector(vector: Vector): Point {
+        return new Point(
+            this.x + vector.x,
+            this.y + vector.y
+        );
+    }
+
+    toString() {
+        return this.x.toString() + ',' + this.y.toString();
+    }
+}
