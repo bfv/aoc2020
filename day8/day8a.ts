7bda5f4247321df9ba64b69ca079dc0da3d7e834
Bronco Oostermeyer <b.oostermeyer@flusso.nl>
2020-12-08 07:56:35 +0100

    missing break

--- day8/day8a.ts
+++ day8/day8a.ts
@@ -35,7 +35,7 @@ function checkLoop(instructions: Instruction[]): number {
 
         switch(instruction.operator) {
             case 'jmp': offset += instruction.argument; break;
-            case 'acc': accumulator += instruction.argument;
+            case 'acc': accumulator += instruction.argument; break;
             case 'nop': offset++; break;
         }
     }
