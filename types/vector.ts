89bb4fd7020ce9195991c9baa1fcfda4ca8e0dc9
Bronco Oostermeyer <b.oostermeyer@flusso.nl>
2020-12-12 14:27:24 +0100

    added operation/NullVector

--- types/vector.ts
+++ types/vector.ts
@@ -1,7 +1,34 @@
+import { Matrix } from "./matrix";
+import { Point } from "./point";
 
 export class Vector {
 
     constructor (public x: number, public y: number) {}
 
+    multiply(amount: number): Vector {
+        return new Vector(this.x * amount, this.y * amount);
+    }
+
+    getVector(from: Point, to: Point): Vector {
+        return new Vector(
+            to.x - from.x,
+            to.y - from.y
+        );
+    }
+
+    rotate(degrees: number): Vector {
+        return this.toMatrix().rotate(degrees).toVector();
+    }
+
+    toMatrix(): Matrix {
+        let matrix = new Matrix(1, 2);
+        matrix.data = [[this.x, this.y]];
+        return matrix;
+    }
+
+    toString(): string {
+        return `${this.x}, ${this.y}`;
+    }
 }
 
+export const NullVector = new Vector(0, 0);
